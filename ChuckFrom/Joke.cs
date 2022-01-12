7477b9a0fef9c65b1ded862f6d1d8531c871cb9f
Opilane TTHK <opilane@tthk.ee>
2022-01-12 09:38:48 +0200

    chuck

---
+++ ChuckFrom/Joke.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckFrom
+{
+    class Joke
+    {
+        public value value { get; set; }
+    }
+    public class value
+    {
+        public string joke { get; set; }
+    }
+}
